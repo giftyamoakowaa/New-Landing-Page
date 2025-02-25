9f8a423189e8fd6be8ae59f7b573e778b6cebcaf
giftyamoakowaa <maameamoakowaa1@gmail.com>
2025-02-25 18:56:50 +0000

    p

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ app.listen(4800, ()=>
 
 )
 
-function toggleMenu() {
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".mobile-menu");
     const navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("active");
-}
 
-// Attach the function to the menu button click event
-document.querySelector(".mobile-menu").addEventListener("click", toggleMenu);
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
