3ec3115d2057325e8ac45a7fc591dfef87ff3abb
giftyamoakowaa <maameamoakowaa1@gmail.com>
2025-02-25 22:17:55 +0000

    more

--- src/index.js
+++ src/index.js
@@ -17,3 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle("active");
     });
 });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const text = document.getElementById("innovator-text");
+//     const viewMoreBtn = document.getElementById("view-more-btn");
+  
+//     let expanded = false;
+//     viewMoreBtn.addEventListener("click", function () {
+//       if (!expanded) {
+//         text.style.maxHeight = "none"; // Show full text
+//         viewMoreBtn.textContent = "View Less";
+//       } else {
+//         text.style.maxHeight = "80px"; // Shorten text
+//         viewMoreBtn.textContent = "View More";
+//       }
+//       expanded = !expanded;
+//     });
+//   });
+  
